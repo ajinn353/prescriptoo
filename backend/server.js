979f7cb08bcd8e181ef1185e65adf9a01e15ed05
ajin <ajinn712@gmail.com.com>
2025-04-24 12:42:52 +0530

    server changed

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: "https://prescriptoo-xhav.onrender.com",
+    origin: 'https://prescriptoo-frontend.onrender.com',
     methods: ["GET", "POST","PUT","DELETE"],
     allowedHeaders: ["Content-Type"]
 }));
@@ -20,16 +20,14 @@ app.listen(3300,()=>{
     
 })
 
-const connectdb = async ()=>{
-    try{
-        mongoose.connect('mongodb+srv://ajinnn712:wggD12dihWked8vI@cluster0.wqr9jge.mongodb.net/prescription?retryWrites=true&w=majority&appName=Cluster0')       
-        console.log(' MongoDB connected successfully');
-        
+const connectdb = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://ajinnn712:wggD12dihWked8vI@cluster0.wqr9jge.mongodb.net/prescription?retryWrites=true&w=majority&appName=Cluster0');
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error);
     }
-    catch(error){
-         console.log(error);
-         
-    }
-}
+};
+
 
 connectdb();
